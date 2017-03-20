0b666d0605987ad5897614a2c78540a24ba27aa5
Mal Graty <mal.graty@googlemail.com>
2017-03-20 13:46:19 +0000

    Pre-package MergeStrategy

---
+++ src/main/scala/sbtassembly/Log4j2MergeStrategy.scala
@@ -0,0 +1,29 @@
+package sbtassembly
+
+import java.io.{FileOutputStream, File}
+
+import scala.collection.JavaConverters.asJavaEnumerationConverter
+
+import org.apache.logging.log4j.core.config.plugins.processor.PluginCache
+
+object Log4j2MergeStrategy {
+  val plugincache: MergeStrategy = new MergeStrategy {
+    val name = "log4j2::plugincache"
+    def apply(tempDir: File, path: String, files: Seq[File]): Either[String, Seq[(File, String)]] = {
+      val file = MergeStrategy.createMergeTarget(tempDir, path)
+      val out = new FileOutputStream(file)
+
+      val aggregator = new PluginCache()
+      val filesEnum = files.toIterator.map(_.toURI.toURL).asJavaEnumeration
+
+      try {
+          aggregator.loadCacheFiles(filesEnum)
+          aggregator.writeCache(out)
+          Right(Seq(file -> path))
+      }
+      finally {
+          out.close()
+      }
+    }
+  }
+}
